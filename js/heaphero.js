acf7337c2fbf51c169dce0995af482d23b990f31
Benjamin Schwartz <49853598+wasabiidev@users.noreply.github.com>
2021-03-15 19:15:45 -0400

    initial commit

---
+++ js/heaphero.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true, 
+    path: 'https://heap.io/wp-content/themes/rs_theme/heaphero.json'
+})
