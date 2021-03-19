86f6fe694380d0bba0a2c128bad0726b54bee991
Benjamin Schwartz <49853598+wasabiidev@users.noreply.github.com>
2021-03-19 13:29:58 -0400

    changed design

--- js/heaphero.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true, 
-    path: 'https://heap.io/wp-content/themes/rs_theme/heaphero.json'
-})
