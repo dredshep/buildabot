ef1e3107c08917ecbcf93ecbeee919bc7b5547fd
dredshep <dredshep@gmail.com>
2021-03-29 02:02:58 -0400

    broken

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+switch (localStorage.getItem('nk_theme')) {
+  case "night":
+    // User has night mode cookies
+    $('.morelogo img').attr("src", "/images/buildabot-logo.png")
+    break;
+  case "day":
+    // User has day mode cookies
+    $('.morelogo img').attr("src", "/images/buildabot-logo-light-mode.png")
+    break;
+  default:
+    // User has no theme cookies
+    $('.morelogo img').attr("src", "/images/buildabot-logo-light-mode.png")      
+}
+
 $('.t-day').click(() => {
   // Turn to light mode
-  $('.logo img').attr("src", "/images/buildabot-logo-light-mode.png")
+  $('.morelogo img').attr("src", "/images/buildabot-logo-light-mode.png")
 })
 
 $('.t-night').click(() => {
   // Turn to dark mode
-  $('.logo img').attr("src", "/images/buildabot-logo.png")
+  $('.morelogo img').attr("src", "/images/buildabot-logo.png")
 })
